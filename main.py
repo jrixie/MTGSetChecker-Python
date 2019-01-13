b02d76c6fb68a59dbca58ae385763b24c5cc7818
Jay Rixie <jayrixie@gmail.com>
2019-01-12 23:59:44 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from mtgsdk import Card
+from mtgsdk import Set
+from mtgsdk import Type
+from mtgsdk import Supertype
+from mtgsdk import Subtype
+from mtgsdk import Changelog
+
+def main():
+    card = Card.find(386616)
+    print(card.name)
+
+
+if __name__ == "__main__":
+    main()
